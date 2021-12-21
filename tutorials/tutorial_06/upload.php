07ee7b66179a554b7035259dc06d6b5d8eb6c344
Soethandarhtunkyi <soethandarhtunkyi@gmail.com>
2021-12-21 10:24:47 +0630

    [master] Adding tuto7 and review of tuto6

---
+++ tutorials/tutorial_06/upload.php
@@ -0,0 +1,23 @@
+<?php
+$foldername = $_POST["foldername"];
+$location = $foldername . "/";
+$fileName = $_FILES["file"]["name"];
+$tmp = $_FILES["file"]["tmp_name"];
+$type = $_FILES["file"]["type"];
+if (isset($fileName)) {
+    $structure = "./" . $foldername;
+    if (!mkdir($structure)) {
+        echo "Folder Already Existed.<br>";
+    }
+    if (!empty($fileName)) {
+        if ($type == "image/jpg" || $type == "image/jpeg" || $type == "image/png") {
+            if (move_uploaded_file($tmp, $location . $fileName)) {
+                echo "File Uploaded to " . $foldername;
+            } else {
+                echo "Not Uploaded  " . $foldername;
+            }
+        } else {
+            echo "Not Uploaded" . $foldername;
+        }
+    }
+}
