da76847005773555407af84ea08dcebd324fedb1
Soethandarhtunkyi <soethandarhtunkyi@gmail.com>
2022-01-06 17:11:09 +0630

    [master] search date in laravel

---
+++ laravel/import/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Student\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Redirect to student list
+Route::get('/', function () {
+    return redirect()->route('students.create');
+});
+
+// Students list resource route
+Route::resource('students', StudentController::class);
+
+
+Route::get('/file-import',[StudentController::class,'importView'])->name('import-view');
+Route::post('/import',[StudentController::class,'import'])->name('import');
+Route::get('/export-students',[StudentController::class,'export'])->name('export');
---
+++ laravel/laravel-between-two-dates/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', function () {
+    if (request()->start_date || request()->end_date) {
+        $start_date = Carbon::parse(request()->start_date)->toDateTimeString();
+        $end_date = Carbon::parse(request()->end_date)->toDateTimeString();
+        $data = App\Models\Student::whereBetween('created_at',[$start_date,$end_date])->get();
+    } else {
+        $data = App\Models\Student::latest()->get();
+    }
+    
+    return view('welcome', compact('data'));
+});
